589bb1e01db005e0a06ac92c7cee8137bbeb3939
karne07 <d3mirors.07@gmail.com>
2026-04-21 14:21:10 +0300

    commit

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Varsa eski konteynerleri durdur ve temizle
-docker compose down || true
+sudo docker compose down || true
 
 # En güncel imajı DockerHub'dan çek
-docker compose pull
+sudo docker compose pull
 
 # Uygulamayı arka planda başlat
-docker compose up -d
+sudo docker compose up -d
 
 # Kullanılmayan eski imajları temizle (disk dolmaması için)
-docker image prune -f
+sudo docker image prune -f
